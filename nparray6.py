5fa8f9d659b0a939c71a7e7cddde9bf10e07551e
Sujatasahu27 <sujata17sahu@gmail.com>
2026-06-02 16:40:40 +0530

    Update nparray6.py

--- nparray6.py
+++ nparray6.py
@@ -1,7 +1,6 @@
 import numpy as np
 
 # Generate a random array of shape (2, 5) 
-# Using random.rand for continuous floats between 0 and 1
 random_array = np.random.rand(2, 5)
 
 print("Random Array of shape (2, 5):\n", random_array)
